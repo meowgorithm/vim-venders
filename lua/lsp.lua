fbc8ff199761de401716ff605f17a49f427da9f7
Christian Rocha <christian@rocha.is>
2021-08-02 17:46:23 -0400

    Fix LspInstall config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -119,11 +119,20 @@ vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
 -- LSPInstall
 --
 
-require'lspinstall'.setup() -- important
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    require'lspconfig'[server].setup{}
+  end
+end
 
-local autoServers = require'lspinstall'.installed_servers()
-for _, server in pairs(servers) do
-  require'lspconfig'[server].setup{}
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers()    -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
 
 --
