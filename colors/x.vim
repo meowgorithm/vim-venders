936cc569e7f055d1d7810ab41ccd702dde366fe0
Christian Rocha <christian@rocha.is>
2022-06-22 13:14:09 -0400

    WIP color theme + use vim-go for better Go syntax highlighting

--- colors/x.vim
+++ colors/x.vim
@@ -1,23 +1,49 @@
+hi NonText guifg=#414141
 hi LineNr guifg=#505050
 hi CursorLine guibg=#0B0B0B
 hi CursorLineNr guifg=#868686 guibg=none gui=none
 hi SignColumn guibg=none
 hi ColorColumn guibg=#202020
 
+hi Folded guifg=#767676 guibg=#2a2a2a ctermfg=243 ctermbg=236
+
 hi PMenu guibg=#333333
 
-hi NonText guifg=#414141
+hi VertSplit guifg=#262626 guibg=#585858
 
-hi Normal guifg=#dddddd
-hi Comment guifg=#666666 gui=none
-hi Constant guifg=#dddddd gui=italic,underline
-hi Identifier guifg=#dddddd gui=bold,italic
-hi Statement guifg=#dddddd gui=italic
-hi PreProc guifg=#dddddd gui=underline
-hi Type guifg=#dddddd gui=bold
-hi Special guifg=#dddddd gui=bold,underline
+hi String guifg=#B18A6D
+hi Comment guifg=#565656
+hi SpecialComment guifg=#cc0000 guibg=#0000cc
+hi Constant guifg=#FF8787 gui=bold
+hi Character guifg=#999999
+hi Number guifg=#999999
+hi Boolean guifg=#FF8686 gui=bold
+hi Float guifg=#999999
+hi Function guifg=#12AAFF
+hi Identifier guifg=#3EF5F5
+hi Conditional guifg=#FF9ACD
+hi Statement guifg=#18D37E
+hi Repeat guifg=#999999
+hi Label guifg=#FF87D7
+hi Operator guifg=#FF5F87
+hi Keyword guifg=#FF61DC
+hi Exception guifg=#999999
+hi Include guifg=#999999
+hi PreProc guifg=#999999
+hi Define guifg=#999999
+hi Macro guifg=#999999
+hi PreConduit guifg=#999999
+hi StorageClass guifg=#999999
+hi Type guifg=#FFC75A
+hi Structure guifg=#FFFF87
+hi Typedef guifg=#999999
+hi Tag guifg=#999999
+hi Special guifg=#8470FF
+hi SpecialChar guifg=#cc0000 guibg=#0000cc
+hi Delimiter guifg=#999999
+hi Debug guifg=#999999
 
-hi Error guibg=#FA3973 guifg=#f1f1f1 gui=none
+hi Error guibg=#FA3973 guifg=#f1f1f1
 hi Warning guifg=#e0af68
 hi Information guifg=#0db9d7
 hi Hint guifg=#00ffff
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,6 +11,41 @@ packer.startup(function(use)
 	use("preservim/nerdcommenter")
 	use("tpope/vim-fugitive")
 	use("jamessan/vim-gnupg")
+	use({
+		"fatih/vim-go",
+		config = function()
+			vim.cmd([[
+                let g:go_highlight_functions = 1
+                let g:go_highlight_function_parameters = 1
+                let g:go_highlight_function_calls = 1
+                let g:go_highlight_operators = 1
+                let g:go_highlight_methods = 1
+                let g:go_highlight_structs = 1
+                let g:go_highlight_trailing_whitespace_error = 1
+                let g:go_highlight_space_tab_error = 1
+                let g:go_highlight_types = 1
+                let g:go_highlight_extra_types = 1
+                let g:go_highlight_chan_whitespace_error = 1
+                let g:go_highlight_array_whitespace_error = 1
+                let g:go_highlight_build_constraints = 1
+                let g:go_highlight_generate_tags = 1
+                let g:go_highlight_fields = 1
+                let g:go_highlight_string_spellcheck = 1
+                let g:go_highlight_format_strings = 1
+                let g:go_highlight_varaible_declarations = 1
+                let g:go_highlight_varaible_assignments = 1
+                let g:go_auto_sameids = 0
+                let g:go_auto_type_info = 0
+                let g:go_fmt_autosave = 0
+                let g:go_imports_autosave = 0
+                let g:go_fmt_options = {}
+                let g:go_mod_fmt_autosave = 0
+                let g:go_metalinter_autosave = 0
+                let g:go_template_autocreate = 0
+                let g:go_jump_to_error = 0
+            ]])
+		end,
+	})
 
 	use({
 		"vim-scripts/BufOnly.vim",
